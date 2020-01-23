dc7558f62cafe3713fc76175bd7d95c5c13a5e9b
ZacharyAb <abramsonzach@gmail.com>
2020-01-23 16:25:40 -0700

    Set up server dependencies and start script. Create gitignore and env files.  # Please enter the commit message for your changes. Lines starting

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const env = require(dotenv).config();
+const port = process.env.PORT;
+
+
+app.use(express.static('./public'));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Tuning into ${port}`);
+})
