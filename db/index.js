599a7b73d6c2a022aec633d61dc44365d9800038
ZacharyAb <abramsonzach@gmail.com>
2020-01-23 17:45:36 -0700

    Connected to database and set up error handling

--- db/index.js
+++ db/index.js
@@ -1,1 +1,11 @@
-// database index.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/reviews');
+
+var db = mongoose.connection;
+db.on('error', () => {
+  console.log('Error connecting to database')
+})
+
+db.once('open', () => {
+  console.log('You\'ve connected to your database');
+})
