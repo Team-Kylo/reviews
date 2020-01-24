c8d6ecf2293c30c59a45d2063bd9913f22cda12d
ZacharyAb <abramsonzach@gmail.com>
2020-01-23 17:01:36 -0700

    Finish with react set up and test

--- server/client/components/app.jsx
+++ server/client/components/app.jsx
@@ -1,3 +1,16 @@
 // This is my react app
 import React from 'react';
-import reactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+  return (<div>
+    This is a test for react
+  </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('components')
+)
