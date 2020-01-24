83b1639b7c88fabbf1f649249b8d41117f289da0
ZacharyAb <abramsonzach@gmail.com>
2020-01-24 16:06:07 -0700

    Create first route to respond with a review body upon a get request

--- server/index.js
+++ server/index.js
@@ -3,10 +3,21 @@ const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const port = process.env.PORT;
+const db = require('../db/index.js');
+let Reviews = require('../db/schema.js');
+
 
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+
+// a route to request all review objects from the db
+app.get('/reviews', (req, res) => {
+  db.schema.Reviews.find({})
+    .then((dbObj) => {
+      res.send(dbObj[Math.ceil(Math.random() * 100)]);
+  });
+})
 
 app.listen(port, () => {
   console.log(`Tuning into ${port}`);
