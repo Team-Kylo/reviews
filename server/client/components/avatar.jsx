ff1165eb3ee3d204c70e583342dcc7d244d1d232
ZacharyAb <abramsonzach@gmail.com>
2020-01-29 15:52:39 -0700

    Set up a few tests, some still failing. Still working out url params.

--- server/client/components/avatar.jsx
+++ server/client/components/avatar.jsx
@@ -1,12 +1,18 @@
 // This is your avatar image component
 import React from 'react';
-
+import styled from 'styled-components';
 const axios = require('axios');
 
-const AvatarImg = ({image}) => (
-  <div className="avatarImg">
-    <img src={image} alt="" />
-  </div>
-);
+const Avatar = styled.div`
+  border-radius: 1.3em;
+  height: 50px;
+  width: 50px;
+`;
+
+const AvatarImg = ({image}) => {
+  <Avatar className="avatarStyles">
+    <img src={image} alt="A national treasure" />
+  </Avatar>
+};
 
 export default AvatarImg;
---
+++ server/client/components/avatar.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { shallow } from 'enzyme';
+import styled from 'styled-components';
+import AvatarImg from './avatar';
+
+describe('<AvatarImg />', () => {
+  it('should return null as a stateless component', () => {
+    const wrapper = shallow(<AvatarImg />);
+    expect(wrapper.instance()).toBe(null);
+  });
+});
